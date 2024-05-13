48a3351b4c7a198048aeca61bc65b6b45ed98e6a
KinzelVA <168530439+KinzelVA@users.noreply.github.com>
2024-05-14 00:48:41 +0700

    add dopolnitelnoe.py

--- dopolnitelnoe.py
+++ dopolnitelnoe.py
@@ -1,12 +1,4 @@
-"""1. Создайте базовый класс `Animal`, который будет содержать общие атрибуты (например, `name`, `age`) и методы (`make_sound()`, `eat()`) для всех животных.
-
-2. Реализуйте наследование, создав подклассы `Bird`, `Mammal`, и `Reptile`, которые наследуют от класса `Animal`. Добавьте специфические атрибуты и переопределите методы, если требуется (например, различный звук для `make_sound()`).
-
-3. Продемонстрируйте полиморфизм: создайте функцию `animal_sound(animals)`, которая принимает список животных и вызывает метод `make_sound()` для каждого животного.
-
-4. Используйте композицию для создания класса `Zoo`, который будет содержать информацию о животных и сотрудниках. Должны быть методы для добавления животных и сотрудников в зоопарк.
-
-5. Создайте классы для сотрудников, например, `ZooKeeper`, `Veterinarian`, которые могут иметь специфические методы (например, `feed_animal()` для `ZooKeeper` и `heal_animal()` для `Veterinarian`)."""
+import pickle
 
 class Animal:
    def __init__(self, name, age):
@@ -53,6 +45,15 @@ class Zoo:
         for staff in self.staff:
             print(f"{staff.name}, a {staff.__class__.__name__.lower()}.")
 
+    def save_to_file(self, filename):
+        with open(filename, "wb") as file:
+            pickle.dump((self.animals, self.staff),file)
+
+    def load_from_file(self, filename):
+        with open(filename, "rb") as file:
+            self.animals, self.staff = pickle.load(file)
+
+
 class ZooKeeper:
     def __init__(self, name):
         self.name = name
@@ -93,13 +94,13 @@ zoo.show_staff()
 zookeeper.feed_animal(mammal)
 veterinarian.heal_animal(bird)
 
+               #сохранение текущего состояния зоопарка
+zoo.save_to_file("zoo_state.pkl")
 
-
-
-
-
-
-
+new_zoo = Zoo()
+new_zoo.load_from_file("zoo_state.pkl")
+new_zoo.show_animals()
+new_zoo.show_staff()
 
 
 
